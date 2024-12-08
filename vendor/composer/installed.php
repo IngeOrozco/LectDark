ae269f191f973ecba10abf12ac7caf77aa05b29e
IngeOrozco <lect.orozco@gmail.com>
2024-12-07 21:32:40 -0500

    composer ajust x2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lect-studios/lectdark',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2b8b7c2e125e01974a080f350147c63133a2fa69',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lect-studios/lectdark' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2b8b7c2e125e01974a080f350147c63133a2fa69',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
